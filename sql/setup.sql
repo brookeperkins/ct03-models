079f6b39ae8016b393186c9b590c39c0666db370
brookeperkins <brookemmperkins@gmail.com>
2020-09-08 17:30:58 -0700

    transport test passes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,17 @@
 DROP TABLE IF EXISTS cats;
+DROP TABLE IF EXISTS transports;
 
 CREATE TABLE cats (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name TEXT NOT NULL,
   age INT CHECK (age > 0),
   weight TEXT
+);
+
+CREATE TABLE transports (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  wheels INT CHECK (wheels > 0),
+  color TEXT
 );
 
