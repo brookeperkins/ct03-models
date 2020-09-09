fbaaab23e7ce4d55555155cdb94e3610592be96f
brookeperkins <brookemmperkins@gmail.com>
2020-09-08 19:10:06 -0700

    post and delete routes tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,41 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/cats');
 
 describe('03-models routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('creates a new cat via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/cats')
+      .send({ name: 'Nomi', age: 5, weight: '16 lbs' });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      name: 'Nomi',
+      age: 5,
+      weight: '16 lbs'
+    });
+  });
+
+  it('deletes a cat by id via DELETE', async() => {
+    const createdCat = await Cat.insert({
+      name: 'Nomi',
+      age: 5,
+      weight: '16 lbs'
+    });
+
+    const response = await request(app)
+      .delete(`/api/v1/cats/${createdCat.id}`);
+
+    expect(response.body).toEqual({
+      id: createdCat.id,
+      name: 'Nomi',
+      age: 5,
+      weight: '16 lbs'
+    });
   });
 });
